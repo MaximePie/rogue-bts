23ccec2494794653a09687de7829aeca35aa4952
Maxime Pie <maxime.pie.mail@gmail.com>
2022-12-02 18:15:39 +0100

    config: add tests suite

--- src/__tests__/firstTest.test.ts
+++ src/__tests__/firstTest.test.ts
@@ -4,14 +4,24 @@
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/svelte";
 import Counter from "../lib/Counter.svelte";
+import App from "../App.svelte";
+
+/**
+ * @jest-environment jsdom
+ */
+test("should render", () => {
+    const results = render(App );
+    expect(() => results.getByText("Vite + Svelte")).not.toThrow();
+});
 
 describe('First tests', () => {
     it('should pass', () => {
         expect(true).toBe(true);
     });
 
+
     it ('Should display the Counter button', () => {
         const { getByText } = render(Counter);
-        expect(getByText('count is')).toBeInTheDocument();
+        expect(getByText('count is 0')).toBeInTheDocument();
     });
 });
